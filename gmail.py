29cbff9a95266603907bedaf1191fce8d3dc0c19
Nelly-ayebale <ayebalenelly26@gmail.com>
2024-07-29 13:51:02 +0200

    Initial Commit-added a statistics file

---
+++ gmail.py
@@ -0,0 +1,31 @@
+import time
+import smtplib
+from dotenv import load_dotenv
+from email.message import EmailMessage
+import os
+
+load_dotenv()
+
+def email_alert(subject, body, to):
+    msg = EmailMessage()
+    msg.set_content(body)
+
+    gmail_user = os.getenv("GMAIL_USER")
+    gmail_password = os.getenv("GMAIL_PASSWORD")
+
+    print(f"GMAIL_USER: {gmail_user}")
+    print(f"GMAIL_PASSWORD: {gmail_password}")
+
+    msg['Subject'] = subject
+    msg['From'] = gmail_user
+    msg['To'] = to
+
+    connection = smtplib.SMTP('smtp.gmail.com', 587)
+    connection.ehlo()
+    connection.starttls()
+    connection.login(user=gmail_user, password=gmail_password)
+    connection.send_message(msg)
+    connection.quit()
+
+if __name__ == '__main__':
+    email_alert("Testing","https://www.linkedin.com/in/nelly-ayebale-4864b0202/","ayebalenelly26@gmail.com")
